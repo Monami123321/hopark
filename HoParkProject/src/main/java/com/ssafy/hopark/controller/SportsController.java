888e1d7ed189c5fc7764cb1b625befe9f6715475
Monami123321 <mmonami123321@gmail.com>
2023-11-22 09:37:49 +0900

    seachCondition DTO 추가, 운동 검색 기능 추가

--- HoParkProject/src/main/java/com/ssafy/hopark/controller/SportsController.java
+++ HoParkProject/src/main/java/com/ssafy/hopark/controller/SportsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,10 +37,12 @@ public class SportsController {
 		return new ResponseEntity<List<Sports>>(list, HttpStatus.OK);
 
 	}
-
-	@GetMapping("search")
+	@ApiOperation(value = "전달받은 검색조건에 따라 운동 리스트를 반환합니다.", notes = "SportsSearchCondition에 딱 맞는 객체만 동적쿼리로 조회하여 반환합니다.(모두 AND 조건)")
+	@PostMapping("search")
 	public ResponseEntity<?> searchByCondition(@RequestBody SportsSearchCondition searchCondition) {
 		List<Sports> list = sportsService.searchByCondition(searchCondition);
+		
+		System.out.println(searchCondition.toString());
 
 		if (list == null || list.size() == 0) {
 			return new ResponseEntity<String>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
