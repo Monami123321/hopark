34b03cf7bcb9e612e5e1abf8c9539afc89b97377
blogog <h103906@naver.com>
2023-11-22 17:06:39 +0900

    mimi vue folder upload

--- mimi/vue-project/src/App.vue
+++ mimi/vue-project/src/App.vue
@@ -1,22 +1,34 @@
 <template>
   <div>
-    <header>
+    <!-- <header>
       <nav>
         <RouterLink to="/">Home</RouterLink>
       </nav>
-    </header>
-    <button @click="select">직접 선택하기</button>
-    <RouterView/>
+    </header> -->
+    <div class="position-absolute end-0 bottom-0">
+      <button class="mx-4" @click="select">직접 선택하기</button>
+      <button @click="select">FFIT 추천받기</button>
+    </div>
+    <!-- <RouterView/> -->
+    <Sidebar></Sidebar>
   </div>
 </template>
 
 <script setup>
-
+  import Sidebar from '@/components/common/Sidebar.vue';
 </script>
 
 <style>
   body {
     background-image: url("@/assets/main_bg.jpg");
     background-position: 0% 10%;
+  }
+  button {
+    background-color: #73D0BB;
+    border: none;
+    border-radius: 30px;
+    color: white;
+    padding: 15px 20px;
+    font-size: 20px;
   }
 </style>
--- mimi/vue-project/src/main.js
+++ mimi/vue-project/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios' // axios 전역 사용 위해 import
+import 'bootstrap/dist/css/bootstrap.min.css' // 부트스트랩사용을 위해 전역 주입
 
 import App from './App.vue'
 import router from './router'
