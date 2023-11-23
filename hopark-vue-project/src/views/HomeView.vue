3ef42ca101933c913249a08f4a937b3dc8784500
blogog <h103906@naver.com>
2023-11-23 11:10:33 +0900

    mimi

--- hopark-vue-project/src/views/HomeView.vue
+++ hopark-vue-project/src/views/HomeView.vue
@@ -5,7 +5,9 @@
         <RouterLink to="/">Home</RouterLink>
       </nav>
     </header> -->
-
+    <div class="bgImg">
+      <img src="@/assets/logo.png" />
+    </div>
     <div class="position-absolute end-0 bottom-0">
       <router-link :to="{name: 'select'}" class="mx-4 mimibutton">직접 선택하기</router-link>
       <router-link :to="{name: 'recommend'}" class="mimibutton">FFIT 추천받기</router-link>
@@ -20,10 +22,15 @@
   import {RouterLink, RouterView} from 'vue-router'
 </script>
 
-<style scoped>
-  body {
+<style>
+  div.img {
+    
+  }
+  .bgImg {
     background-image: url("@/assets/main_bg.jpg");
-    background-position: 0% 10%;
+    width: 1920px;
+    height: 1080px;
+    /* background-position: 0% 10%; */
   }
   .mimibutton {
     background-color: #73D0BB;
