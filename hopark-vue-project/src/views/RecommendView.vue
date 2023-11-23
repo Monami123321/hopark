9729240ffb79273771183a36d1c53eb06ed5862e
blogog <h103906@naver.com>
2023-11-23 09:24:53 +0900

    recommend page update

---
+++ hopark-vue-project/src/views/RecommendView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>무료 운동 성향 테스트</h1>
+        <div>
+            
+        </div>
+        <hr>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
